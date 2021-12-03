df9c39b72595fd1f435e3f8bc24bc4b7c4f2ef4a
Rafael Corsi <rafael.corsi@insper.edu.br>
2021-12-03 15:03:11 -0300

    v0

---
+++ README.md
@@ -0,0 +1,165 @@
+# 21b - Avaliação Prática 3
+
+Leia atentamente as informações a seguir:
+
+1. **Faça o seu trabalho de maneira ética!**
+1. **A cada 30 minutos você deverá fazer um commit no seu código!**
+    - Códigos que não tiverem commit a cada 30min ou que mudarem drasticamente entre os commits serão descartados (conceito I) !!
+    - Você deve inserir mensagens condizentes nos commits!
+1. Duração: 3h
+1. Usar o código exemplo deste repositório, ele já possui:
+    - Diplay do módulo OLED1
+    - TC/ RTT/ RTC/ AFEC já adicionados no ASF Wizard
+    - Deve utilizar o rtos e suas funcionalidades.
+
+:triangular_flag_on_post: **Ao finalizar a entrega preencher o formulário (uma única vez!):**
+
+Acessar a avaliacao pelo link:
+
+## Descrição
+
+![](img.png)
+
+Nessa avaliação assíncrona vocês irão trabalhar com o LVGL e um encoder para desenvolver uma interface de um rádio automotivo. O protótipo deve usar o encoder para a seleção da frequência/ volume (conceito C>) e exibir na tela a frequência atual, além disso deve possibilitar o usuário armazenar até nove estações.
+
+### LVGL
+
+O código exemplo já possui a tela do LVGL montada como indicado a seguir:
+
+![](lvgl.png)
+
+#### Label
+
+Vocês podem alterar o label do `dial` acessando diretamente o label que foi declarado como global.  
+
+#### Botões
+
+Todos os botões foram mapeados para o `event_handler`, nessa função de callback eu recupero qual o ID do botão apertado (`id`) e qual evento que foi detectado (`code`): 
+
+- LV_EVENT_VALUE_CHANGED: Botão apertado
+- LV_EVENT_LONG_PRESSED: Botão pressionado por um tempo maior
+
+ATENÇÃO: quando detectado um `LV_EVENT_LONG_PRESSED` o LVGL gera também um evento de botão apertado `LV_EVENT_VALUE_CHANGED`.
+
+> DICA RTOS: Você vai ter que colocar esses dados em uma fila para processar na task_update.
+
+Para testar a detecção dos eventos execute o código fornecido analisando a saida do terminal (COM), e aperte ao apertar o botão 1 e soltar você deve obter algo como: 
+
+```
+ID 1: EVENT 28
+```
+
+E se apertar e segurar:
+
+```
+ID 1: EVENT 28
+ID 1: EVENT 5
+```
+
+### Enconder
+
+> Imagens extraídas do site (boa referência para vocês) https://lastminuteengineers.com/rotary-encoder-arduino-tutorial/
+
+![](encoder.png)
+
+O enconder é um dispositivo eletromecânico que converte rotação mecânica em pulsos elétricos. Eles são muito utilizados para medir posição ou velocidade de motores e também em interfaces homem máquina de sistemas embarcados. O encoder é diferente do potenciômetro que vocês utilizaram nas outras entregas, pois ele gera um sinal DIGITAL em formato de pulsos (o pot é uma resistência elétrica variável, nós liamos ele como um sinal analógico). O enconder tem uma grande vantagem em relação ao potenciômetro: ele não tem fim de curso. É possível dar 360 com o enconder, algo que não é possível com o pot normal.
+
+O enconder que iremos utilizar possui duas saídas: CLK (A) e DT (B) e gera pulsos distintos conforme o sentido de rotação (horário/ anti-horário), como mostrado nas figuras a seguir:
+
+- Se DT (B) != CLK (A) então houve um pulso no sentido horário
+
+![](https://lastminuteengineers.com/wp-content/uploads/arduino/rotary-encoder-output-pulses-in-clockwise-rotation.png)
+
+- Se DT (B) == CLK (A) então houve um pulso no sentido anti-horário
+
+![](https://lastminuteengineers.com/wp-content/uploads/arduino/rotary-encoder-output-pulses-in-anticlockwise-rotation.png)
+
+> A entrada CLK (A) serve como CLOCK da leitura do sinal, sempre que houver uma variação (0-->1/ 1-->0) fazemos a leitura do sinal DT (B) para descobrir o sentido de rotação. 
+
+#### Ligação
+
+Nós sugerimos a seguinte ligação do enconder com a placa:
+
+- CLK: ligado no PC31 do EXT1  
+- DT: ligado no PA19 do EXT1
+- SW: desconectado 
+- +: no 3v3 do kit
+- GND: no terra do kit
+
+![](ligacao.png)
+
+Para ler os sinais do PC31 e PA19 vocês precisam configurar os pinos como entrada **SEM PULLUP**.
+
+> DICA 1: Use interrupção de borda no pino do CLK (A) na do DT não é necessário
+> DICA 2: Eu sugiro ativarem debounce nos pinos com frequência de 200Hz. Exemplo:
+>
+> `pio_set_debounce_filter(DT_PIO, DT_PIO_PIN_MASK, 200);`)
+
+#### Lendo o sinal
+
+A leitura do sinal deve ser feita no callback do pino de clock, a cada interrupção você deve obter: 
+
+- o valor atual de CLK
+- o valor atual de DT
+
+E então fazer a lógica descrita anteriormente para identificar o sentido de rotação.
+
+> DICA RTOS: Você vai ter que colocar a direção detectada em um fila para processar na task_update.
+
+#### Lendo a velocidade 
+
+Além da leitura da direção de rotação do enconder vocês terão que classificar a velocidade de rotação, a ideia aqui é que vocês terão que mudar a frequência em 0.1 Mhz se o enconder for girado de vagar e 10 Mhz se for detectado que o usuário está girando rapidamente o botão. Para que possa varrer o range de frequências de maneira prática.
+
+Para isso vocês terão que utilizar o RTT (como incremento ou apenas ele como relógio), calculando o dT entre os pulsos para saber se foi rápido ou devagar.
+
+> DICA RTOS: Envie a velocidade de rotação detectada junto com a direção na fila.
+
+### Operação
+
+Especificação: 
+
+- Frequencia mínima: 77.5
+- Frequencia máxima: 107.9 
+- Step rotação lenta: 0.1
+- Step rotação rápida: 5
+
+Ao iniciar a aplicação todos os botões devem ter frequência associada de 77.5 Mhz, o usuário pode usando o enconder escolher uma nova frequência e se for de seu interesse armazenar a frequência associando ela a ao ID de um dos botões.
+
+Ao apertar e segurar (evento: LONG_PRESSED) um dos botões o sistema deve armazenar a frequência que estava sendo exibida nesse botão, ao apertar e soltar rapidamente um dos botões o sistema deve exibir a frequência que foi salva nele (a padrão ou a nova).
+
+## Rubrica
+
+A seguir rubrica da entrega.
+
+### C
+
+Assista ao vídeo no youtube para ver o comportamento da máquina:
+
+
+Requisitos funcionais:
+
+- Mudar frequência de acordo com sentido de rotação (encoder)
+- Dois modos de incremento da freq: rápido (passo de 5.0) e lento (passo de 0.1)
+- Ao apertar o botão exibir a frequência salva no index do botão
+- Ao apertar e segurar o botão salvar a frequência atual como padrão do botão
+
+Requisitos técnicos:
+
+- Só atualizar display na task_update
+- Usar IRQ no CLK do encoder para leitura dos pulsos
+- Usar RTT para estimar se a rotação foi do tipo rápida ou lenta
+- RTOS: Usar fila para comunicar callback do clk e task_update (enviando sentido e velocidade de rotação)
+- RTOS: Usar fila para comunicar handler dos botões do LVGL com a task_update (passando ID e tipo de evento)
+
+## C+ - Novas velocidades
+
+- Sistema funciona sem nenhum bug.
+- Criar novas categorias de velocidade para facilitar a navegação:
+    - Rapidão: 10 passos
+    - Mega rápido: 100 passos
+
+## B - ??
+
+## B+ - 
+
+## A - 
---
+++ firmware/src/main.c
@@ -0,0 +1,302 @@
+/************************************************************************/
+/* includes                                                             */
+/************************************************************************/
+
+#include <asf.h>
+#include <string.h>
+#include "ili9341.h"
+#include "lvgl.h"
+#include "touch/touch.h"
+
+/************************************************************************/
+/* LCD / LVGL                                                           */
+/************************************************************************/
+
+#define LV_HOR_RES_MAX          (320)
+#define LV_VER_RES_MAX          (240)
+
+/*A static or global variable to store the buffers*/
+static lv_disp_draw_buf_t disp_buf;
+
+/*Static or global buffer(s). The second buffer is optional*/
+static lv_color_t buf_1[LV_HOR_RES_MAX * LV_VER_RES_MAX];
+static lv_disp_drv_t disp_drv;          /*A variable to hold the drivers. Must be static or global.*/
+static lv_indev_drv_t indev_drv;
+
+/************************************************************************/
+/* HW                                                                   */
+/************************************************************************/
+
+#define LED_PIO       PIOC
+#define LED_PIO_ID    ID_PIOC
+#define LED_IDX       8u
+#define LED_IDX_MASK  (1u << LED_IDX)
+
+#define CLK_PIO     PIOC
+#define CLK_PIO_ID  ID_PIOC
+#define CLK_PIO_PIN 31
+#define CLK_PIO_PIN_MASK (1 << CLK_PIO_PIN)
+
+#define DT_PIO     PIOA
+#define DT_PIO_ID  ID_PIOA
+#define DT_PIO_PIN 19
+#define DT_PIO_PIN_MASK (1 << DT_PIO_PIN)
+
+#define BTN_PIO     PIOA
+#define BTN_PIO_ID  ID_PIOA
+#define BTN_PIO_PIN 0
+#define BTN_PIO_PIN_MASK (1 << BTN_PIO_ID)
+
+/************************************************************************/
+/* RTOS                                                                 */
+/************************************************************************/
+
+#define TASK_LCD_STACK_SIZE                (1024*6/sizeof(portSTACK_TYPE))
+#define TASK_LCD_STACK_PRIORITY            (tskIDLE_PRIORITY)
+
+extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,  signed char *pcTaskName);
+extern void vApplicationIdleHook(void);
+extern void vApplicationTickHook(void);
+extern void vApplicationMallocFailedHook(void);
+extern void xPortSysTickHandler(void);
+
+extern void vApplicationStackOverflowHook(xTaskHandle *pxTask, signed char *pcTaskName) {
+	printf("stack overflow %x %s\r\n", pxTask, (portCHAR *)pcTaskName);
+	for (;;) {	}
+}
+
+extern void vApplicationIdleHook(void) { }
+
+extern void vApplicationTickHook(void) { }
+
+extern void vApplicationMallocFailedHook(void) {
+	configASSERT( ( volatile void * ) NULL );
+}
+
+/************************************************************************/
+/* global                                                               */
+/************************************************************************/
+
+static lv_obj_t * dial;
+static lv_obj_t * btnm1;
+
+#define FREQ_MIN 77.5f
+#define FREQ_MAX 107.9f
+#define FREQ_STEP 0.1f
+
+/************************************************************************/
+/* prototype                                                            */
+/************************************************************************/
+
+static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses);
+
+/************************************************************************/
+/* callback/ handler                                                    */
+/************************************************************************/
+
+void RTT_Handler(void) {
+	uint32_t ul_status;
+
+	/* Get RTT status - ACK */
+	ul_status = rtt_get_status(RTT);
+
+	/* IRQ due to Time has changed */
+	if ((ul_status & RTT_SR_RTTINC) == RTT_SR_RTTINC) {
+	
+	}
+
+	/* IRQ due to Alarm */
+	if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
+		
+	}
+}
+
+
+/************************************************************************/
+/* lvgl                                                                 */
+/************************************************************************/
+
+static void event_handler(lv_event_t * e) {
+
+	lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * obj = lv_event_get_target(e);
+	
+	if( (code == LV_EVENT_VALUE_CHANGED) || (code == LV_EVENT_LONG_PRESSED) ) {
+		uint32_t id = lv_btnmatrix_get_selected_btn(obj);
+        const char * txt = lv_btnmatrix_get_btn_text(obj, id);
+		printf("ID %d: EVENT %d\n", id, code);
+	}
+}
+
+static const char * btnm_map[] = {"0", "1", "2", "3", "\n",
+								  "5", "6", "7", "8", "\n",
+								  "Volume", "Dial", ""};
+
+void lv_som(void) {
+	btnm1 = lv_btnmatrix_create(lv_scr_act());
+	lv_btnmatrix_set_map(btnm1, btnm_map);
+	lv_btnmatrix_set_btn_ctrl_all(btnm1, LV_BTNMATRIX_CTRL_NO_REPEAT);
+	lv_btnmatrix_set_btn_width(btnm1, 8, 2);        /*Make "Action1" twice as wide as "Action2"*/
+	lv_btnmatrix_set_btn_ctrl(btnm1, 8, LV_BTNMATRIX_CTRL_CHECKED);
+	lv_btnmatrix_set_btn_ctrl(btnm1, 9, LV_BTNMATRIX_CTRL_CHECKED);
+	lv_obj_align(btnm1, LV_ALIGN_BOTTOM_MID, 0, -10);
+	lv_obj_add_event_cb(btnm1, event_handler, LV_EVENT_ALL, NULL);
+	
+	dial = lv_label_create(lv_scr_act());
+	lv_obj_set_style_text_font(dial,  &lv_font_montserrat_42, LV_STATE_DEFAULT);
+	lv_obj_set_style_text_align(dial, LV_TEXT_ALIGN_CENTER, 0);
+	lv_obj_align(dial, LV_ALIGN_CENTER, 0, -80);
+}
+
+/************************************************************************/
+/* TASKS                                                                */
+/************************************************************************/
+
+static void task_lcd(void *pvParameters) {
+	lv_som();
+	
+	for (;;)  {
+		lv_tick_inc(50);
+		lv_task_handler();
+		vTaskDelay(50);
+	}
+}
+
+static void task_update(void *pvParameters) {
+	
+	float freq = FREQ_MIN;
+	float freq_list[10];
+
+	lv_label_set_text_fmt(dial, "%.2f", freq);
+
+	for (;;)  {
+		
+	}
+}
+
+/************************************************************************/
+/* configs                                                              */
+/************************************************************************/
+
+static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses) {
+	uint32_t ul_previous_time;
+
+	/* Configure RTT for a 1 second tick interrupt */
+	rtt_sel_source(RTT, false);
+	rtt_init(RTT, pllPreScale);
+	
+	ul_previous_time = rtt_read_timer_value(RTT);
+	while (ul_previous_time == rtt_read_timer_value(RTT));
+	
+	rtt_write_alarm_time(RTT, IrqNPulses+ul_previous_time);
+
+	/* Enable RTT interrupt */
+	NVIC_DisableIRQ(RTT_IRQn);
+	NVIC_ClearPendingIRQ(RTT_IRQn);
+	NVIC_SetPriority(RTT_IRQn, 4);
+	NVIC_EnableIRQ(RTT_IRQn);
+	rtt_enable_interrupt(RTT, RTT_MR_ALMIEN | RTT_MR_RTTINCIEN);
+}
+
+static void configure_lcd(void) {
+	/**LCD pin configure on SPI*/
+	pio_configure_pin(LCD_SPI_MISO_PIO, LCD_SPI_MISO_FLAGS);  //
+	pio_configure_pin(LCD_SPI_MOSI_PIO, LCD_SPI_MOSI_FLAGS);
+	pio_configure_pin(LCD_SPI_SPCK_PIO, LCD_SPI_SPCK_FLAGS);
+	pio_configure_pin(LCD_SPI_NPCS_PIO, LCD_SPI_NPCS_FLAGS);
+	pio_configure_pin(LCD_SPI_RESET_PIO, LCD_SPI_RESET_FLAGS);
+	pio_configure_pin(LCD_SPI_CDS_PIO, LCD_SPI_CDS_FLAGS);
+	
+	ili9341_init();
+	ili9341_backlight_on();
+}
+
+static void configure_console(void) {
+	const usart_serial_options_t uart_serial_options = {
+		.baudrate = USART_SERIAL_EXAMPLE_BAUDRATE,
+		.charlength = USART_SERIAL_CHAR_LENGTH,
+		.paritytype = USART_SERIAL_PARITY,
+		.stopbits = USART_SERIAL_STOP_BIT,
+	};
+
+	/* Configure console UART. */
+	stdio_serial_init(CONSOLE_UART, &uart_serial_options);
+
+	/* Specify that stdout should not be buffered. */
+	setbuf(stdout, NULL);
+}
+
+/************************************************************************/
+/* port lvgl                                                            */
+/************************************************************************/
+
+void my_flush_cb(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p) {
+	ili9341_set_top_left_limit(area->x1, area->y1);   ili9341_set_bottom_right_limit(area->x2, area->y2);
+	ili9341_copy_pixels_to_screen(color_p,  (area->x2 + 1 - area->x1) * (area->y2 + 1 - area->y1));
+	
+	/* IMPORTANT!!!
+	* Inform the graphics library that you are ready with the flushing*/
+	lv_disp_flush_ready(disp_drv);
+}
+
+void my_input_read(lv_indev_drv_t * drv, lv_indev_data_t*data) {
+	int px, py, pressed;
+	
+	if (readPoint(&px, &py))
+	data->state = LV_INDEV_STATE_PRESSED;
+	else
+	data->state = LV_INDEV_STATE_RELEASED;
+	
+	data->point.x = px;
+	data->point.y = py;
+}
+
+void configure_lvgl(void) {
+	lv_init();
+	lv_disp_draw_buf_init(&disp_buf, buf_1, NULL, LV_HOR_RES_MAX * LV_VER_RES_MAX);
+	
+	lv_disp_drv_init(&disp_drv);            /*Basic initialization*/
+	disp_drv.draw_buf = &disp_buf;          /*Set an initialized buffer*/
+	disp_drv.flush_cb = my_flush_cb;        /*Set a flush callback to draw to the display*/
+	disp_drv.hor_res = LV_HOR_RES_MAX;      /*Set the horizontal resolution in pixels*/
+	disp_drv.ver_res = LV_VER_RES_MAX;      /*Set the vertical resolution in pixels*/
+
+	lv_disp_t * disp;
+	disp = lv_disp_drv_register(&disp_drv); /*Register the driver and save the created display objects*/
+	
+	/* Init input on LVGL */
+	lv_indev_drv_init(&indev_drv);
+	indev_drv.type = LV_INDEV_TYPE_POINTER;
+	indev_drv.read_cb = my_input_read;
+	lv_indev_t * my_indev = lv_indev_drv_register(&indev_drv);
+}
+
+/************************************************************************/
+/* main                                                                 */
+/************************************************************************/
+int main(void) {
+	/* board and sys init */
+	board_init();
+	sysclk_init();
+	configure_console();
+
+	/* LCd, touch and lvgl init*/
+	configure_lcd();
+	configure_touch();
+	configure_lvgl();
+
+	/* Create task to control oled */
+	if (xTaskCreate(task_lcd, "LCD", TASK_LCD_STACK_SIZE, NULL, TASK_LCD_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create lcd task\r\n");
+	}
+	
+	/* Create task to control oled */
+	if (xTaskCreate(task_update, "UPDATE", TASK_LCD_STACK_SIZE, NULL, TASK_LCD_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create lcd update\r\n");
+	}
+	
+	/* Start the scheduler. */
+	vTaskStartScheduler();
+
+	while(1){ }
+}
